0e3a10cd585c53e530a9d8d17ffdd52816455362
mateusalvesc <mateus.alvesc02@gmail.com>
2022-05-16 18:28:52 -0300

    Initial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Button, Paper, TextField } from '@mui/material';
+
+export default function Form({ addTodo }) {
+  const [text, setText] = useState(null);
+  const [id, setId] = useState(0);
+  
+  const todoCreate = (text) => {
+    const todoObj = { text: text, id: id };
+    setId(id + 1);
+    addTodo(todoObj);
+    document.getElementById('outlined-basic').value = null;
+  };
+
+  return (
+    <Paper style={{ padding: '1em' }}>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <TextField 
+      id='outlined-basic' 
+      label='Tarefa' 
+      variant='outlined' 
+      onChange={(e) => setText(e.target.value)}
+      fullWidth 
+      />
+      <Button variant='text' onClick={() => todoCreate(text)}>
+        Add
+      </Button>
+      </div>
+    </Paper>
+  );
+}
